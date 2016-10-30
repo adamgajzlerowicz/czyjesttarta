c1719c461a1215720c68189e8c949b39b4b996a1
Adam Gajzlerowicz <adam@Adams-MacBook-Air.local>
2016-10-30 10:22:51 +0100

    Updated radium props

--- src/components/Bits.jsx
+++ src/components/Bits.jsx
@@ -1,6 +1,5 @@
 import {render} from 'react-dom'
 import React from 'react';
-import Radium from 'radium';
 
 export const Result = ({state, isLoading}) => {
     const resultStyle = {
@@ -22,40 +21,3 @@ export const Result = ({state, isLoading}) => {
         </div>
     )
 };
-
-const ButtonsRaw = ({onAdd, onSub, isLoading, state}) => {
-    const transitionsPositive = {
-        transition: '1s ease-out',
-        ':hover': {
-            animation: 'x 3s ease 0s infinite',
-            color:'#DAEFB3'
-        }
-    };
-    const clickStyle = {
-        padding: 20,
-        color: 'black',
-        fontSize: '70px'
-    };
-    const buttonsContainerStyle = {
-        display: isLoading ? 'none' : 'block'
-    };
-    const transitionsNegative = {
-        transition: '1s ease-out',
-        ':hover': {
-            animation: 'x 3s ease 0s infinite',
-            color:'#EF3E36'
-        }
-    };
-    return (
-        <div style={buttonsContainerStyle}>
-            <a href="#" style={[clickStyle, transitionsNegative]} onClick={()=> {
-                onSub()
-            }} key={1}>-</a>
-            <span style={clickStyle} key={2}>{state}</span>
-            <a href="#" style={[clickStyle, transitionsPositive]} onClick={()=> {
-                onAdd()
-            }} key={3}>+</a>
-        </div>
-    )
-};
-export const Buttons = Radium(ButtonsRaw);
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import {render} from 'react-dom'
 import React from 'react';
 import Radium from 'radium';
-import {Result, Buttons} from './Bits';
+import {Result} from './Bits';
 
 const UnstyledMain = ({onAdd, onSub, ...props}) => {
     const state = props.kawalki;
@@ -30,20 +30,54 @@ const UnstyledMain = ({onAdd, onSub, ...props}) => {
         justifyContent: 'space-around',
         alignItems: 'center'
     };
+
+
+    const clickStyle = {
+        padding: 20,
+        color: 'black',
+        fontSize: '70px'
+    };
+
+    const buttonsContainerStyle = {
+        display: isLoading ? 'none' : 'block'
+    };
+
     const loaderStyle = {
         display: !isLoading ? 'none' : 'block',
         color: 'white',
         fontSize: 25
     };
 
+    const transitionsPositive = {
+        transition: '1s ease-out',
+        ':hover': {
+            animation: 'x 3s ease 0s infinite',
+            color:'#DAEFB3'
+        }
+    };
 
+    const transitionsNegative = {
+        transition: '1s ease-out',
+        ':hover': {
+            animation: 'x 3s ease 0s infinite',
+            color:'#EF3E36'
+        }
+    };
 
     return (
         <div style={containerStyle}>
             <div style={innerContainerStyle}>
                 <Result state={state} isLoading={isLoading} />
-                <Buttons onAdd={onAdd} onSub={onSub} isLoading={isLoading} state={state}/>
-                <div style={loaderStyle}>
+                <div style={buttonsContainerStyle}>
+                    <a key={1} href="#" style={[clickStyle, transitionsNegative]} onClick={()=> {
+                        onSub()
+                    }}>-</a>
+                    <span key={2} style={clickStyle}>{state}</span>
+                    <a href="#" style={[clickStyle, transitionsPositive]} onClick={()=> {
+                        onAdd()
+                    }}>+</a>
+                </div>
+                <div style={loaderStyle} key={3}>
                     ...loading
                 </div>
             </div>
