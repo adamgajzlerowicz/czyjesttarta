829e0d362239224d1ae4c5f74e0e5aad8b41b0e2
Adam Gajzlerowicz <adam@Adams-MacBook-Air.local>
2017-04-09 20:21:34 +0200

    Ava working ok

--- src/components/Bits.spec.js
+++ src/components/Bits.spec.js
@@ -3,11 +3,10 @@ import Bits from './Bits';
 import React from 'react';
 import { mount } from 'enzyme';
 
-
 test('Test Bits component', t => {
     const wrapper = mount(<Bits state={1} isLoading={false} />);
     t.is(wrapper.props().state, 1, 'props actually passed');
-    t.is(wrapper.find('p'), 'Jest tarta :)');
+    t.is(wrapper.find('p').text(), 'Jest tarta :)');
     wrapper.setProps({ state: 0 });
-    t.is(wrapper.find('p'), 'Nie ma tarty :(');
+    t.is(wrapper.find('p').text(), 'Nie ma tarty :(');
 });
